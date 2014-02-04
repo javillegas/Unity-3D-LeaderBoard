c64833b50b75ae0d7517fc88395be9197f8bea6a
Akshay Mishra <akshay.mishra@shephertz.co.in>
2014-02-04 14:55:37 +0530

    sample updated.
    Signed-off-by: Akshay Mishra <akshay.mishra@shephertz.co.in>

--- App42_Unity_Sample_C#/Assets/scripts/Constants.cs
+++ App42_Unity_Sample_C#/Assets/scripts/Constants.cs
@@ -5,7 +5,7 @@ public class Constants
 {
 	public string apiKey  ="YOUR_API_KEY";						// API key that you have receieved after the success of app creation from AppHQ
 	public string secretKey ="YOUR_SECRET_KEY";					// SECRET key that you have receieved after the success of app creation from AppHQ
-
+	
 	public string gameName ="GameName";						// Name of the game which you can create from AppHQ console by clicking 
-																		// Business Service -> Game Service -> Game -> Add Game
+	// Business Service -> Game Service -> Game -> Add Game
 }
